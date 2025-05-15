086f0f07dd3cd5408ef47acd8ae9ca991f832414
Akio706 <fifaisaak@gmail.com>
2025-05-15 05:06:58 +0300

    new

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from nicegui import ui
-from db import initialize_db
+from nicegui import app, ui
+
+# Эта строка обязательно должна быть до ui.run()
+app.add_static_files('/static', 'static')
+
+# Ваш роутинг:
 from pages.main import main_page
 from pages.fields import fields_page
 from pages.map import map_page
-
-initialize_db()
 
 @ui.page('/')
 def _():
