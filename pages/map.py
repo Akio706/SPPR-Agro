ea0714e3d060856f08b3121ec134636b3a6f672a
Akio706 <fifaisaak@gmail.com>
2025-05-15 05:17:19 +0300

    new

--- pages/map.py
+++ pages/map.py
@@ -60,7 +60,7 @@ draw_control = {
     },
 }
 
-m = ui.leaflet(center=(55.75, 37.62), draw_control=draw_control, hide_drawn_items=True)
+m = ui.leaflet(center=(55.75, 37.62), draw_control=draw_control)
 m.on('draw:created', handle_draw)
 
 ui.button('Назад к полям', on_click=lambda: ui.open('/fields')).classes('mt-4')
